2627eb56a9c1068ae51ba7e5ff1dbcb461c2abd6
adeade95 <57157852+adeade95@users.noreply.github.com>
2020-08-03 23:56:07 +0200

    sembra funzionare e spedire stringhe velocemente
    sembra funzionare e spedire stringhe anche abbastanza velocemente

--- sensori_server.ino
+++ sensori_server.ino
@@ -18,18 +18,18 @@ IPAddress secondaryDNS(8, 8, 4, 4); //optional
 // Create AsyncWebServer object on port 80
 AsyncWebServer server(80);
 
-int ningressi = 2;  //numero totale di ingressi, alla fine sulla scheda ne dovremmo prevedere almeno 12 e 4 uscite
-int pin[2]={16,12};  //numero dei pin in ingresso
-int statetrigger[2]={2,2}; //salvo gli stati momentaneamente letti e il trigger, riservo i primi posti per gli stati e i successivi per iltrigger rispettivi
-int statorip[2]={1,1}; // dove salvo lo stato che mi definisce il riposo
-int statoattuale[2]={2,2};  //dove salvo lo stato attuale
-char statetriggerchar[3]="22";  //salvo gli stati momentaneamente letti e il trigger, riservo i primi posti per gli stati e i successivi per iltrigger rispettivi
-char stateattualechar[3]="22";  //dove salvo lo stato attuale che poi stamperò
-char statoripchar[3]="22";  // dove salvo lo stato che mi definisce il riposo che poi potrò stampare
+int ningressi = 12;  //numero totale di ingressi, alla fine sulla scheda ne dovremmo prevedere almeno 12 e 4 uscite
+int ingpin[12]={16,12,16,12,16,12,16,12,16,12,16, 12};  //numero dei pin in ingresso
+int statetrigger[12]={2,2,2,2,2,2,2,2,2,2,2,2}; //salvo gli stati momentaneamente letti e il trigger, riservo i primi posti per gli stati e i successivi per iltrigger rispettivi
+int statorip[12]={1,1,1,1,1,1,1,1,1,1,1,1}; // dove salvo lo stato che mi definisce il riposo
+int statoattuale[12]={3,3,3,3,3,3,3,3,3,3,3,3};  //dove salvo lo stato attuale
+char statetriggerchar[13]="222222222222";  //salvo gli stati momentaneamente letti e il trigger, riservo i primi posti per gli stati e i successivi per iltrigger rispettivi
+char stateattualechar[13]="333333333333";  //dove salvo lo stato attuale che poi stamperò
+char statoripchar[13]="222222222222";  // dove salvo lo stato che mi definisce il riposo che poi potrò stampare
 
 String ttext="3"; //definizione variabile per lo stato dell'ingresso
 
-void ConnectToWiFi()
+void ConnectToWiFi()  //funzione per connettersi al wifi
 {
  
   WiFi.mode(WIFI_STA);
@@ -58,15 +58,29 @@ WiFi.config(local_IP, gateway, subnet, primaryDNS, secondaryDNS);
   Serial.println(WiFi.localIP());
 }
 
+int setinputpin(int* inputpins, int npins){
+  Serial.println("settaggio pin inpin input");
+  for(int i=0; i<npins;i++){
+  pinMode(inputpins[i], INPUT);    // sets input pin
+  digitalWrite(inputpins[i], HIGH);// pull up alto
+  Serial.print("fase");
+  Serial.print(i);
+  Serial.print(" settato input pin");
+  Serial.println(inputpins[i]);              //avvisiamo di quello che facciamo sulla porta seriale
+  }
+    return 1;
+}
+
+
 void resettrigger(int numcaselle, int *vettrigger){ //funzione per resettare vettore trigger
   int i=0; //indice per scorrere il vettore
   for(i=0;i<=numcaselle;i++)
-    vettrigger[i]=0;
+    vettrigger[i]=2;
   }
 
   
 int lettf1(int npin){ //funzione lettura pin fase 1
-    int vlett=2;
+    int vlett=2;  //così dovrei eventualmente scoprire se qui va storto qualcosa
     if(digitalRead(npin)==LOW){
       Serial.print(npin);
       Serial.println(" stato basso"); 
@@ -82,7 +96,7 @@ int lettf1(int npin){ //funzione lettura pin fase 1
 int lettf2(int numtotpin, int *numpin, int *statirippin, int *statitriggerf, int *statoattualef){  //funzione lettura ingresso e cambiamento stato trigger
   int i=0;  //indice per scorrere il vettore
   for(i=0;i<=numtotpin;i++){
-    statirippin[i]=lettf1(numpin[i]);
+    statoattualef[i]=lettf1(numpin[i]); //leggo lo stato dei pin
     if(statirippin[i]!=statoattualef[i])
       statitriggerf[i]=1;
     }
@@ -91,9 +105,9 @@ int lettf2(int numtotpin, int *numpin, int *statirippin, int *statitriggerf, int
 
 int arrayinttochar(int* intarray, char* chararray, int numcar){ //converte array di nt in array di char poi convertiremo il vrttore char in string 
   for(int i=0; i<numcar; i++){
-    if(intarray[i]=0)
+    if(intarray[i]==0)
       chararray[i]='0';
-    if(intarray[i]=1)
+    if(intarray[i]==1)
       chararray[i]='1';
     if(intarray[i]!=0 && intarray[i]!=1)
       chararray[i]='2';
@@ -109,26 +123,28 @@ int arraychartostring (char *chararray, String stringa){  // NON USARE converte 
 
 void setup() 
 {
-  Serial.begin(115200);
+  Serial.begin(115200);   //inizializzo porta seriale
  
-  ConnectToWiFi();
+  ConnectToWiFi();        //mi connetto al wifi
   /*
   // Create AsyncWebServer object on port 80
   Serial.println("creazione server su porta 80");
   AsyncWebServer server(80);
   Serial.println("creato server su porta 80");*/
   Serial.println("settaggio pin inpin input");
-  pinMode(inpin, INPUT);    // sets the digital pin 4 led pin as output
+  pinMode(inpin, INPUT);    // sets input pin
   digitalWrite(inpin, HIGH);// pull up alto
   Serial.println("settato inpin input");
+  setinputpin(ingpin, ningressi);   //setto tutti gli ingressi come tali
 
     //ricezione richiesta info stato sensori
    server.on("/state", HTTP_GET, [](AsyncWebServerRequest *request){
-   if(lettf2(ningressi, pin, statorip, statetrigger, statoattuale))
+   if(lettf2(ningressi, ingpin, statorip, statetrigger, statoattuale))
       Serial.println("ricevuta richiesta STATE e ho appena letto i pin adesso converto");
     else
      Serial.println("ricevuta richiesta STATE e ho appena letto MALE i pin adesso converto");
     arrayinttochar(statoattuale, stateattualechar, ningressi);
+    Serial.println(String(stateattualechar));
     ttext=String(stateattualechar);
     request->send_P(200, "text/plain", ttext.c_str());
   });
@@ -146,15 +162,16 @@ void setup()
   
     //reset triigger
    server.on("/resettrigger", HTTP_GET, [](AsyncWebServerRequest *request){
-    resettrigger(ningressi,statetrigger); //reset trigger----------------------------------------------
-    ttext=String(lettf1(inpin));  //per poi  stampare
+    Serial.println("ricevuta richiesta RESETSTATETRIGGER eseguo");
+    resettrigger(ningressi, statetrigger);  //resetto array int
+    arrayinttochar(statetrigger,statetriggerchar,ningressi);  //lo converto in array char
+    ttext=String(statetriggerchar);  //per poi  stampare
     request->send_P(200, "text/plain", ttext.c_str());
   });
 
   server.begin(); //inizializzazione server
 }
 
-    //ricezione richiesta reset trigger da creare-----------------------------------
     
 void loop(){
   
